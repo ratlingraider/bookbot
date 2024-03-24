8554d9b23c95f99417a41cd7cacb5a8df9903e3a
ratlingraider <121434491+ratlingraider@users.noreply.github.com>
2024-03-24 07:11:56 -0400

    Finished BookBot Project

--- main.py
+++ main.py
@@ -12,10 +12,17 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_letters = get_num_letters(text)
+    report_list = create_report(num_letters)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(f"{num_letters} letters found in the document") 
+    print()
 
+    for item in report_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
+    print("--- End report ---")
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -27,13 +34,22 @@ def get_book_text(path):
 def get_num_letters(text):
     all_freq = {}
     words = text.split()
-    letters = ''.join(words)
-    lower_letters = letters.lower()
-    for i in lower_letters:
+    characters = ''.join(words)
+    lower_characters = characters.lower()
+    for i in lower_characters:
         if i in all_freq:
             all_freq[i] += 1
         else:
             all_freq[i] = 1
     return all_freq
 
+def create_report(num_letters):
+    report_list = []
+    for letter in num_letters:
+        report_list.append({"char": letter, "num": num_letters[letter]})
+    report_list.sort(reverse=True, key=sort_on)
+    return report_list
+
+def sort_on(d):
+    return d["num"]
 main()
